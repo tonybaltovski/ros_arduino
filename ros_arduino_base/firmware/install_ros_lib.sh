fffb58c491b8d143f242af3f87dff6af9239d178
Tony Baltovski <tony.baltovski@gmail.com>
2015-03-04 03:00:27 -0500

    Towards using ros-control starting with firmware.

--- ros_arduino_base/firmware/install_ros_lib.sh
+++ ros_arduino_base/firmware/install_ros_lib.sh
@@ -3,6 +3,5 @@ cd "$(dirname "$0")"/libraries
 rm -rf *
 rosrun rosserial_arduino make_libraries.py .
 git clone https://github.com/tonybaltovski/PololuMC33926MotorDriver.git
-wget http://www.pjrc.com/teensy/arduino_libraries/Encoder.zip
-unzip Encoder.zip
-rm Encoder.zip
+git clone https://github.com/tonybaltovski/DFRobotL298PShieldDriver.git
+git clone https://github.com/PaulStoffregen/Encoder.git
--- ros_arduino_base/firmware/two_wheel_base/two_wheel_base.ino
+++ ros_arduino_base/firmware/two_wheel_base/two_wheel_base.ino
@@ -31,9 +31,10 @@
 
 #include <ros.h>
 #include <ros/time.h>
+#include <ros/duration.h>
 #include <ros_arduino_base/UpdateGains.h>
-#include <ros_arduino_msgs/Encoders.h>
-#include <ros_arduino_msgs/CmdDiffVel.h>
+#include <ros_arduino_msgs/BaseFeedback.h>
+#include <ros_arduino_msgs/Drive.h>
 
 /********************************************************************************************
 /                                                     USER CONFIG                           *
@@ -45,6 +46,8 @@
 // Select your motor driver here
 #define PololuMC33926
 //#define DFRobotL298PShield
+
+#define TUNING
 
 // Define your encoder pins here.
 // Try to use pins that have interrupts
@@ -69,11 +72,13 @@
 #include "motor_driver_config.h"
 
 typedef struct {
-  float desired_velocity;     // [m/s]
+  float desired_velocity;     // [radians/s]
+  float estimated_velocity;   // [radians/s]
   uint32_t current_time;      // [milliseconds]
   uint32_t previous_time;     // [milliseconds]
   int32_t current_encoder;    // [counts]
   int32_t previous_encoder;   // [counts]
+  float error;                //
   float previous_error;       // 
   float total_error;          // 
   int16_t command;            // [PWM]
@@ -86,10 +91,6 @@ Encoder right_encoder(RIGHT_ENCODER_A,RIGHT_ENCODER_B);
 
 // Vehicle characteristics
 float counts_per_rev[1];
-float gear_ratio[1];
-int encoder_on_motor_shaft[1];
-float wheel_radius[1];         // [m]
-float meters_per_counts;       // [m/counts]
 int pwm_range[1];
 
 // Gains;
@@ -101,30 +102,26 @@ ControlData left_motor_controller;
 ControlData right_motor_controller;
 
 // Control methods prototypes
-void updateControl(ControlData * ctrl, int32_t encoder_reading);
-void doControl(ControlData * ctrl);
+void doControl(ControlData * ctrl, int32_t encoder_reading);
 void Control();
 
 int control_rate[1];   // [Hz]
-int encoder_rate[1];   // [Hz]
+int feedback_rate[1];  // [Hz]
+int status_rate[1];    // [Hz]
 int no_cmd_timeout[1]; // [seconds]
 
-
-uint32_t up_time;             // [milliseconds]
-uint32_t last_encoders_time;  // [milliseconds]
+uint32_t last_feedback_time;  // [milliseconds]
 uint32_t last_cmd_time;       // [milliseconds]
 uint32_t last_control_time;   // [milliseconds]
-uint32_t last_status_time;    // [milliseconds]
-
 
 // ROS node
-ros::NodeHandle_<ArduinoHardware, 10, 10, 1024, 1024> nh;
+ros::NodeHandle_<ArduinoHardware, 2, 1, 1024, 1024> nh;
 
 // ROS subribers/service callbacks prototye
-void cmdDiffVelCallback(const ros_arduino_msgs::CmdDiffVel& diff_vel_msg); 
+void driveCallback(const ros_arduino_msgs::Drive& drive_msg); 
 
 // ROS subsribers
-ros::Subscriber<ros_arduino_msgs::CmdDiffVel> sub_diff_vel("cmd_diff_vel", cmdDiffVelCallback);
+ros::Subscriber<ros_arduino_msgs::Drive> sub_drive("drive", driveCallback);
 
 // ROS services prototype
 void updateGainsCb(const ros_arduino_base::UpdateGains::Request &req, ros_arduino_base::UpdateGains::Response &res);
@@ -132,11 +129,11 @@ void updateGainsCb(const ros_arduino_base::UpdateGains::Request &req, ros_arduin
 ros::ServiceServer<ros_arduino_base::UpdateGains::Request, ros_arduino_base::UpdateGains::Response> update_gains_server("update_gains", &updateGainsCb);
 
 // ROS publishers msgs
-ros_arduino_msgs::Encoders encoders_msg;
+ros_arduino_msgs::BaseFeedback feedback_msg;
 char frame_id[] = "base_link";
-// ROS publishers
-ros::Publisher pub_encoders("encoders", &encoders_msg);
 
+// ROS publishers
+ros::Publisher pub_feedback("feedback", &feedback_msg);
 
 void setup() 
 { 
@@ -144,10 +141,10 @@ void setup()
   nh.getHardware()->setBaud(BAUD);
   nh.initNode();
 
-  encoders_msg.header.frame_id = frame_id;
+  feedback_msg.header.frame_id = frame_id;
   // Pub/Sub
-  nh.advertise(pub_encoders);
-  nh.subscribe(sub_diff_vel);
+  nh.advertise(pub_feedback);
+  nh.subscribe(sub_drive);
   nh.advertiseService(update_gains_server);
   
   // Wait for ROSserial to connect
@@ -159,11 +156,11 @@ void setup()
 
   if (!nh.getParam("control_rate", control_rate,1))
   {
-    control_rate[0] = 50;
+    control_rate[0] = 100;
   }
-  if (!nh.getParam("encoder_rate", encoder_rate,1))
+  if (!nh.getParam("feedback_rate", feedback_rate,1))
   {
-    encoder_rate[0] = 50;
+    feedback_rate[0] = 50;
   }
   if (!nh.getParam("no_cmd_timeout", no_cmd_timeout,1))
   {
@@ -172,7 +169,7 @@ void setup()
   if (!nh.getParam("pid_gains", pid_gains,3))
   { 
     pid_gains[0] = 150;  // Kp
-    pid_gains[1] =   0;  // Ki
+    pid_gains[1] =   1;  // Ki
     pid_gains[2] =  20;  // Kd
   }
 
@@ -180,32 +177,11 @@ void setup()
   {
     counts_per_rev[0] = 48.0;
   }
-  if (!nh.getParam("gear_ratio", gear_ratio,1))
-  {
-    gear_ratio[0] = 75.0/1.0;
-  }
-  if (!nh.getParam("encoder_on_motor_shaft", encoder_on_motor_shaft,1))
-  {
-    encoder_on_motor_shaft[0] = 1;
-  }
-  if (!nh.getParam("wheel_radius", wheel_radius,1))
-  {
-    wheel_radius[0] = 0.120/2.0;
-  }
   if (!nh.getParam("pwm_range", pwm_range,1))
   {
     pwm_range[0] = 255;
   }
 
-  // Compute the meters per count
-  if (encoder_on_motor_shaft[0] == 1)
-  {
-    meters_per_counts = ((PI * 2 * wheel_radius[0]) / (counts_per_rev[0] * gear_ratio[0]));
-  }
-  else
-  {
-    meters_per_counts = ((PI * 2 * wheel_radius[0]) / counts_per_rev[0]);
-  }
   // Create PID gains for this specific control rate
   Kp = pid_gains[0];
   Ki = pid_gains[1] / control_rate[0];
@@ -218,13 +194,21 @@ void setup()
 
 void loop() 
 {
-  if ((millis() - last_encoders_time) >= (1000 / encoder_rate[0]))
+  if ((millis() - last_feedback_time) >= (1000 / feedback_rate[0]))
   { 
-    encoders_msg.left = left_encoder.read();
-    encoders_msg.right = right_encoder.read();
-    encoders_msg.header.stamp = nh.now();
-    pub_encoders.publish(&encoders_msg);
-    last_encoders_time = millis();
+    feedback_msg.motors[ros_arduino_msgs::BaseFeedback::LEFT].estimated_distance = left_encoder.read() * (2*PI / counts_per_rev[0]);
+    feedback_msg.motors[ros_arduino_msgs::BaseFeedback::LEFT].estimated_velocity = left_motor_controller.estimated_velocity;
+    feedback_msg.motors[ros_arduino_msgs::BaseFeedback::LEFT].fault = left_motor.fault();
+    //feedback_msg.motors[ros_arduino_msgs::BaseFeedback::LEFT].current_draw = left_motor.motor_current();
+   
+    feedback_msg.motors[ros_arduino_msgs::BaseFeedback::RIGHT].estimated_distance = right_encoder.read() * (2*PI / counts_per_rev[0]);
+    feedback_msg.motors[ros_arduino_msgs::BaseFeedback::RIGHT].estimated_velocity = right_motor_controller.estimated_velocity;
+    feedback_msg.motors[ros_arduino_msgs::BaseFeedback::RIGHT].fault = right_motor.fault();
+    //feedback_msg.motors[ros_arduino_msgs::BaseFeedback::RIGHT].current_draw = right_motor.motor_current();
+    
+    feedback_msg.header.stamp = nh.now();
+    pub_feedback.publish(&feedback_msg);
+    last_feedback_time = millis();
   }
   if ((millis()) - last_control_time >= (1000 / control_rate[0]))
   {
@@ -238,74 +222,60 @@ void loop()
     left_motor_controller.desired_velocity = 0.0;
     right_motor_controller.desired_velocity = 0.0;
   }
+  
   nh.spinOnce();
 }
 
 
-void cmdDiffVelCallback( const ros_arduino_msgs::CmdDiffVel& diff_vel_msg) 
+void driveCallback( const ros_arduino_msgs::Drive& drive_msg) 
 {
-  left_motor_controller.desired_velocity = diff_vel_msg.left;
-  right_motor_controller.desired_velocity = diff_vel_msg.right;
+  left_motor_controller.desired_velocity = drive_msg.drivers[ros_arduino_msgs::Drive::LEFT];
+  right_motor_controller.desired_velocity = drive_msg.drivers[ros_arduino_msgs::Drive::RIGHT];
   last_cmd_time = millis();
 }
 
-void updateControl(ControlData * ctrl, int32_t encoder_reading)
+
+void doControl(ControlData * ctrl, int32_t encoder_reading)
 {
   ctrl->current_encoder = encoder_reading;
-  ctrl->current_time = millis();;
-}
+  ctrl->current_time = millis();
+  ctrl->estimated_velocity = (2*PI / counts_per_rev[0]) * (ctrl->current_encoder - ctrl->previous_encoder) * 1000.0 / (ctrl->current_time - ctrl->previous_time);
+  ctrl->error = ctrl->desired_velocity - ctrl->estimated_velocity;
+  ctrl->command += Kp * ctrl->error + Ki * (ctrl->error + ctrl->total_error) + Kd * (ctrl->error - ctrl->previous_error);
 
-void doControl(ControlData * ctrl)
-{
-  float estimated_velocity = meters_per_counts * (ctrl->current_encoder - ctrl->previous_encoder) * 1000.0 / (ctrl->current_time - ctrl->previous_time);
-  float error = ctrl->desired_velocity - estimated_velocity;
-  float cmd = Kp * error + Ki * (error + ctrl->total_error) + Kd * (error - ctrl->previous_error);
-
-  cmd += ctrl->command;
-
-  if(cmd >= pwm_range[0])
+  if(ctrl->command >= pwm_range[0])
   {
-    cmd = pwm_range[0];
+    ctrl->command = pwm_range[0];
   }
-  else if (cmd <= -pwm_range[0])
+  else if (ctrl->command <= -pwm_range[0])
   {
-    cmd = -pwm_range[0];
+    ctrl->command = -pwm_range[0];
   }
   else
   {
-    ctrl->total_error += error;
+    ctrl->total_error += ctrl->error;
   }
 
-  ctrl->command = cmd;
   ctrl->previous_time = ctrl->current_time;
   ctrl->previous_encoder = ctrl->current_encoder;
-  ctrl->previous_error = error;
+  ctrl->previous_error = ctrl->error;
 
 }
 
 void Control()
 {
-  updateControl(&left_motor_controller, left_encoder.read());
-  updateControl(&right_motor_controller, right_encoder.read());
-
-  doControl(&left_motor_controller);
-  doControl(&right_motor_controller);
+  doControl(&left_motor_controller, left_encoder.read());
+  doControl(&right_motor_controller, right_encoder.read());
 
   commandLeftMotor(left_motor_controller.command);
   commandRightMotor(right_motor_controller.command);
-  
 }
 
-void updateGainsCb(const ros_arduino_base::UpdateGains::Request & req, ros_arduino_base::UpdateGains::Response & res)
+void updateGainsCb(const ros_arduino_base::UpdateGains::Request &req, ros_arduino_base::UpdateGains::Response &res)
 {
-  for ( int x = 0; x < 3; x++)
-  {
-    pid_gains[x] = req.gains[x];
-  }
-  
-  Kp = pid_gains[0];
-  Ki = pid_gains[1] / control_rate[0];
-  Kd = pid_gains[2] * control_rate[0];
+  Kp = req.gains[0];
+  Ki = req.gains[1] / control_rate[0];
+  Kd = req.gains[2] * control_rate[0];
 }
 
 
